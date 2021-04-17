5b81f78e0c7c89db5a3faea8260b88ff8d89d943
françois hiriart <f.hiriart@skema.edu>
2021-04-17 16:49:37 +0200

    exo 2

---
+++ db/migrate/20210417101013_add_movie_to_bookmark.rb
@@ -0,0 +1,5 @@
+class AddMovieToBookmark < ActiveRecord::Migration[6.0]
+  def change
+     add_reference :bookmarks, :movie, foreign_key: true
+  end
+end
---
+++ db/migrate/20210417101137_add_list_to_bookmark.rb
@@ -0,0 +1,5 @@
+class AddListToBookmark < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :bookmarks, :list, foreign_key: true
+  end
+end
